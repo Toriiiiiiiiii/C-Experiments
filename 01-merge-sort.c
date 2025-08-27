09580281901e4b0f82e90858f02d5a6e359e3f8e
Tori <toriahall7@gmail.com>
2025-08-27 17:42:08 +0100

    Added comments to Ex#1

--- 01-merge-sort.c
+++ 01-merge-sort.c
@@ -2,18 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Recursive function to perform merge sort
 void msort_iter(int* buf, size_t nmem) {
+	// Base case: If only 1 element, break out
 	if(nmem == 1) return;
 
+	// Split the array into 2 smaller arrays
 	int *left = malloc((nmem+1) / 2 * sizeof(int));
 	int *right = malloc(nmem / 2 * sizeof(int));
 
 	memcpy(left, buf, (nmem+1) / 2 * sizeof(int));
 	memcpy(right, buf+(nmem+1)/2, nmem / 2 * sizeof(int));
 
+	// Perform merge sort on the 2 new arrays
 	msort_iter(left, (nmem+1) / 2);
 	msort_iter(right, nmem / 2);
 
+	// Insert values into the result in order
 	int i = 0, j = 0;
 	int bufIndex = 0;
 	while(i < (nmem+1) / 2 && j < nmem / 2) {
@@ -37,22 +42,26 @@ void msort_iter(int* buf, size_t nmem) {
 		bufIndex++;
 	}
 
+	// If 'right' was emptied first, add the rest of 'left'
 	while(i < (nmem+1) / 2) {
 		buf[bufIndex] = left[i];
 		i++;
 		bufIndex++;
 	}
 
+	// If 'left' was emptied first, add the rest of 'right'
 	while(j < nmem / 2) {
 		buf[bufIndex] = right[j];
 		j++;
 		bufIndex++;
 	}
 
+	// Don't forget to free() what you malloc()!
 	free(left);
 	free(right);
 }
 
+// Wrapper function to sort array
 void msort(int* src, int* buf, size_t nmem) {
 	memcpy(buf, src, nmem * sizeof(int));
 	msort_iter(buf, nmem);
